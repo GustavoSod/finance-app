b379a3ac9cc43df817859c27a4a8d19f38f6dc80
Gustavo Sodré <gustavo.sodre.carreira.rosa@gmail.com>
2024-07-31 00:32:49 -0300

    Add header to home page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,25 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text } from 'react-native';
-import { View } from 'react-native';
+import { Text, Image, View, TouchableOpacity, ScrollView} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import DoubleButton from '@/components/doubleButton';
 import HomeChart from '@/components/homeChart';
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default function HomeScreen() {
   return (
     <View style={{flex:1, backgroundColor: '#151718'}}>
       <StatusBar style='light'/>
       <SafeAreaView>
-        <View className='ml-8 mt-20'>
-          <Text className='text-gray-300 text-lg'>Hello, User!</Text>
-          <Text className='text-gray-300 text-4xl font-semibold'>Famous cryptos 💵</Text>
-        </View>
-        <View className='w-full items-center mt-6'>
-          <DoubleButton/>
-        </View>
-        <View className='mt-2'>
-          <HomeChart/>
-        </View>
+        <ScrollView>
+          <View className='flex flex-row items-center justify-between ms-8 me-8 mt-7'>
+            <View>
+              <Image className='w-12 h-12 rounded-lg' source={require('../../assets/images/person.jpg')}/>
+            </View>
+            <TouchableOpacity className='bg-zinc-800 p-1 rounded-lg'>
+              <MaterialIcons name="notifications-none" size={33} color="#BABABA"/>
+            </TouchableOpacity>
+          </View>
+          <View className='ms-8 mt-8'>
+            <Text className='text-gray-300 text-lg'>Hello, User!</Text>
+            <Text className='text-gray-300 text-4xl font-semibold'>Famous cryptos 💵</Text>
+          </View>
+          <View className='w-full items-center mt-6'>
+            <DoubleButton/>
+          </View>
+          <View className='mt-2'>
+            <HomeChart/>
+          </View>
+        </ScrollView>
       </SafeAreaView>
     </View>
   );
--- components/doubleButton/index.tsx
+++ components/doubleButton/index.tsx
@@ -8,13 +8,13 @@ export default function DoubleButton() {
     <View className='bg-zinc-800 w-[90%] flex-row rounded-xl justify-around items-center p-2'>
       <TouchableOpacity
         onPress={() => setSelected('income')}
-        className={`w-[45%] text-center rounded-xl p-4 items-center justify-center ${selected === 'income' ? 'bg-cyan-600' : 'bg-transparent'}`}
+        className={`w-[45%] text-center rounded-xl p-3 items-center justify-center ${selected === 'income' ? 'bg-cyan-600' : 'bg-transparent'}`}
       >
         <Text className='text-white text-base font-semibold'>Bitcoin</Text>
       </TouchableOpacity>
       <TouchableOpacity
         onPress={() => setSelected('expenses')}
-        className={`w-[45%] text-center rounded-xl p-4 items-center justify-center ${selected === 'expenses' ? 'bg-cyan-600' : 'bg-transparent'}`}
+        className={`w-[45%] text-center rounded-xl p-3 items-center justify-center ${selected === 'expenses' ? 'bg-cyan-600' : 'bg-transparent'}`}
       >
         <Text className='text-white text-base font-semibold'>Ethereum</Text>
       </TouchableOpacity>
